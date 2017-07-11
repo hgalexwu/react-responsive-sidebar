f761e0aff193eb37242eac243a79ba720e4e9c0d
Alexander Wu <alexander.wu@ibm.com>
2017-07-11 14:25:12 -0400

    Fixed build and added bundle

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ var getConfig = require('hjs-webpack');
 
 var config = getConfig({
 
-  isDev: process.env.NODE_ENV !== "production",
+  isDev: false,
   // entry point for the app
-  in: process.env.NODE_ENV !== "production"?'example/main.js':'src/index.js',
+  in: 'src/index.js',
   out: 'dist',
   output: {
     filename: 'bundle.js'
@@ -12,7 +12,5 @@ var config = getConfig({
   clearBeforeBuild: true
 });
 
-if (process.env.NODE_ENV === "production"){
   config.output.libraryTarget = "commonjs2";
-}
 module.exports = config;

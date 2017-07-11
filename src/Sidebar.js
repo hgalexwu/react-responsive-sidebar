362a3be6e7c46788be4f69e160d5f6b57a904a81
Alexander Wu <alexander.wu@ibm.com>
2017-07-11 13:56:34 -0400

    Fixed sidebar item onClick

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -67,7 +67,13 @@ class Sidebar extends Component {
        key: idx,
        hoverHighlight: el.props.hoverHighlight || this.props.hoverHighlight,
        activeHighlight: el.props.activeHighlight || this.props.activeHighlight,
-       onClick: this.onItemSelected,
+       // Fix onclick issue for sidebaritem
+       onClick: (e) => {
+         if (e1.props.onClick){
+           e1.props.onClick(e);
+         };
+         this.onItemSelected(e);
+       }
      }));
     // const content = React.Children.map(this.props.content, React.cloneElement(item, {
     //   hoverHighlight: item.props.hoverHighlight || this.props.hoverHighlight,
